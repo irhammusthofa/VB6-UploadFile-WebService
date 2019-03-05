cdef820d656fdc4aabd5f1f672204b840fc8b884
Irham Mustofa Kamil <irhammusthofa@gmail.com>
2019-03-05 11:34:03 +0700

    Update UploadFileWebserviceFromVB6.vb

--- UploadFileWebserviceFromVB6.vb
+++ UploadFileWebserviceFromVB6.vb
@@ -1,57 +1,62 @@
 
+Public Function getFilename(strFullPath As String) As String
+    On Error Resume Next
+    getFilename = Right(strFullPath, Len(strFullPath) - InStrRev(strFullPath, "\"))
+End Function
+
 Public Function UpLoadData(strFilename As String, strTargetURL As String, Optional namaFile As String) As String
-Dim oHttp As XMLHTTP
-Dim strBody As String
-Dim aPostData() As Byte
-Dim oSignature As String
-Dim strResponse As String
-Dim nFile As Long
-Dim strText As String
+    Dim oHttp As XMLHTTP
+    Dim strBody As String
+    Dim aPostData() As Byte
+    Dim oSignature As String
+    Dim strResponse As String
+    Dim nFile As Long
+    Dim strText As String
 
-On Error GoTo ErrorHandler
+    On Error GoTo ErrorHandler
 
-nFile = FreeFile
-Open strFilename For Binary As #nFile
-strText = String(LOF(nFile), " ")
-Get #nFile, , strText
-Close #nFile
+    nFile = FreeFile
+    Open strFilename For Binary As #nFile
+    strText = String(LOF(nFile), " ")
+    Get #nFile, , strText
+    Close #nFile
 
-Set oHttp = New XMLHTTP
-If namaFile = "" Then
-    namaFile = getFilename(strFilename)
-End If
-oHttp.Open "POST", strTargetURL, False
-oHttp.setRequestHeader "Content-Type", "multipart/form-data, boundary=AaB03x"
-strBody = _
-   "--AaB03x" & vbCrLf & _
-   "Content-Disposition: form-data; name=""filename""" & vbCrLf & vbCrLf & _
-   namaFile & vbCrLf 
-   
-strBody = strBody & "--AaB03x" & vbCrLf & _
-   "Content-Disposition: attachment; name=""gambar""; filename=""" & strFilename & """" & vbCrLf & _
-   "Content-Type: text/plain" & vbCrLf & vbCrLf & _
-   strText & vbCrLf & _
-   "--AaB03x--"
+    Set oHttp = New XMLHTTP
+    If namaFile = "" Then
+        namaFile = getFilename(strFilename)
+    End If
+    oHttp.Open "POST", strTargetURL, False
+    oHttp.setRequestHeader "Content-Type", "multipart/form-data, boundary=AaB03x"
+    strBody = _
+       "--AaB03x" & vbCrLf & _
+       "Content-Disposition: form-data; name=""filename""" & vbCrLf & vbCrLf & _
+       namaFile & vbCrLf 
 
-aPostData = StrConv(strBody, vbFromUnicode)
-oHttp.send aPostData
+    strBody = strBody & "--AaB03x" & vbCrLf & _
+       "Content-Disposition: attachment; name=""gambar""; filename=""" & strFilename & """" & vbCrLf & _
+       "Content-Type: text/plain" & vbCrLf & vbCrLf & _
+       strText & vbCrLf & _
+       "--AaB03x--"
 
-strResponse = oHttp.responseText
-UpLoadData = strResponse
+    aPostData = StrConv(strBody, vbFromUnicode)
+    oHttp.send aPostData
+
+    strResponse = oHttp.responseText
+    UpLoadData = strResponse
 
 
-GoTo LastSub
-On Error GoTo 0
-Exit Function
+    GoTo LastSub
+    On Error GoTo 0
+    Exit Function
 
-ErrorHandler:
-MsgBox "Error : (" & Err.number & ") " & Err.Description, vbExclamation, App.Title
-UpLoadData = "ERROR UPLOAD"
-GoTo LastSub
-Exit Function
+    ErrorHandler:
+    MsgBox "Error : (" & Err.number & ") " & Err.Description, vbExclamation, App.Title
+    UpLoadData = "ERROR UPLOAD"
+    GoTo LastSub
+    Exit Function
 
-LastSub:
-On Error Resume Next
-Set oHttp = Nothing
-On Error GoTo 0
+    LastSub:
+    On Error Resume Next
+    Set oHttp = Nothing
+    On Error GoTo 0
 End Function
